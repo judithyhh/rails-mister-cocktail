397ec2cc4193a849ce04e47bcd9c85068a206863
Judith Hsiang <judithyhh@gmail.com>
2017-08-10 16:01:11 +0800

    Added routes.

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,6 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
-  validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
-  validates :ingredient, presence: true, uniqueness: { scope: :cocktail }
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,4 @@
 class Ingredient < ApplicationRecord
-  has_many :doses, inverse_of: :ingredient
+  has_many :doses
   validates :name, presence: true, uniqueness: { case_sensitive: true }
 end
